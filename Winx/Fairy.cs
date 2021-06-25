90c81daded58b35293522550b9b1c51ef5ec7203
ermeeva_r <ermeeva.r@gmail.com>
2021-06-25 17:07:13 +0300

    first

---
+++ Winx/Fairy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Winx
+{
+    class Fairy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Power { get; set; }
+        public int CountOfLife { get; set; } = 5;
+
+        private Fairy[] fairies;
+
+        public Fairy this[int index]
+        {
+            get
+            {
+                return fairies[index];
+            }
+            set
+            {
+                fairies[index] = value;
+            }
+        }
+
+        public static Fairy operator +(Fairy first, Fairy second)
+        {
+            Fairy fairy = new Fairy();
+            fairy.Name = first.Name + " and " + second.Name;
+            fairy.CountOfLife = first.CountOfLife + second.CountOfLife;
+            fairy.Power = first.Power + second.Power;
+            fairy.Id = 100;
+            return fairy;
+        }
+
+        public static bool operator >(Fairy first, Fairy second)
+        {
+            return first.Power > second.Power;
+        }
+
+        public static bool operator <(Fairy first, Fairy second)
+        {
+            return first.Power < second.Power;
+        }
+    }
+}
---
+++ Winx/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Winx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fairy bloom = new Fairy() { Id = 1, Name = "Bloom", CountOfLife = 10, Power = 3000.00 };
+            Fairy stella = new Fairy() { Id = 2, Name = "Stella", CountOfLife = 7, Power = 2500.00 };
+
+            Console.WriteLine("Сила Блум больше чем у Стеллы: " + (bloom > stella));
+
+            Fairy bloomStella = bloom + stella;
+            Console.WriteLine($"Слияние двух персонажей:" +
+                $"\n\tId: {bloomStella.Id}" +
+                $"\n\tИмя: {bloomStella.Name}" +
+                $"\n\tСила: {bloomStella.Power}" +
+                $"\n\tКоличество жизней: {bloomStella.CountOfLife}");
+        }
+    }
+}
